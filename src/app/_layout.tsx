4b1ed6cdb8f407295c162896f7422f7b5e9d28a5
Humberto Junior <astronomi@gmail.com>
2024-04-02 20:49:52 -0300

    Create global styles for all screens

---
+++ src/app/_layout.tsx
@@ -0,0 +1,31 @@
+import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "../components/loading";
+import "../styles/global";
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
+  return (
+    <>
+      <StatusBar style="light" />
+      {fontsLoaded ? <Slot /> : <Loading />}
+    </>
+  );
+}
