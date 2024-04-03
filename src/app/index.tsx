f7d6a22587e5b96726d0650dc5325cd312289c0e
Humberto Junior <astronomi@gmail.com>
2024-04-03 09:48:54 -0300

    Add icon

---
+++ src/app/index.tsx
@@ -0,0 +1,24 @@
+import { View, Image } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import { Input } from "@/components/input";
+import { colors } from "@/styles/colors";
+
+export default function Home() {
+  return (
+    <View className="flex-1 bg-gray-900 items-center justify-center p-8">
+      <Image source={require("@/assets/logo.png")} />
+
+      <View className="w-full mt-12 pag-3">
+        <Input>
+          <MaterialCommunityIcons
+            name="ticket-confirmation-outline"
+            color={colors.green[200]}
+            size={20}
+          />
+          <Input.Field placeholder="Código do ingresso" />
+        </Input>
+      </View>
+    </View>
+  );
+}
