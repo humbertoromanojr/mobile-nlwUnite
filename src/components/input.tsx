356c2da761bf6753842265fe1bbf941910b50b03
Humberto Junior <astronomi@gmail.com>
2024-04-03 09:28:19 -0300

    Get all properties with ...rest

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,12 +1,24 @@
 import { ReactNode } from "react";
-import { TextInput, View } from "react-native";
+import { TextInput, View, TextInputProps } from "react-native";
+
+import { colors } from "@/styles/colors";
 
 function Input({ children }: { children: ReactNode }) {
-  return <View>{children}</View>;
+  return (
+    <View className="w-full h-14 flex-row items-center gap-3 p-3 border border-green-400 rounded-lg">
+      {children}
+    </View>
+  );
 }
 
-function Field() {
-  return <TextInput />;
+function Field({ ...rest }: TextInputProps) {
+  return (
+    <TextInput
+      className="flex-1 text-white text-base font-regular"
+      placeholderTextColor={colors.gray[200]}
+      {...rest}
+    />
+  );
 }
 
 Input.Field = Field;
