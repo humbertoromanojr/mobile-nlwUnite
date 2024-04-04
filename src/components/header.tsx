f93491ff41a245cd77c4014d772a314b7973d16a
Humberto Junior <astronomi@gmail.com>
2024-04-04 14:54:03 -0300

    Finished header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Header() {
+type HeaderProps = {
+  title: string;
+};
+
+export default function Header({ title }: HeaderProps) {
   return (
-    <View>
-      <Text></Text>
+    <View className="w-full h-28 flex-row items-end bg-black/75 px-8 pb-4 border-b border-white/40">
+      <Text className="flex-1 text-white font-medium text-lg text-center">
+        {title}
+      </Text>
     </View>
   );
 }
